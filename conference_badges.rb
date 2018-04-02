4c2be2ee3c245151896037eb25732fdb8eaa03b9
Ayana Bando <ayanaflax@yahoo.com>
2018-04-02 15:28:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
+
 # Write your code here.
+
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |names|
+    badges.push("Hello, my name is #{names}.")
+  end
+  return badges
+end
+
+def assign_room(attendees)
+  attendees = []
+  counter = 1
+  attendees.each do |names|
+    badges.push("Hello, #{names}. You'll be assigned to room #{counter}")
+    counter += 1
+  end
+  return badges
+end
+end
